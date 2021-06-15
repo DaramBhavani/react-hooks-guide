5e35af3f27a65a4d613bc28c463fe1ecb759cc38
daram.bhavani@gmail.com <daram.bhavani@gmail.com>
2021-06-15 19:29:43 +0800

    learned about the child-to-parent component communication

--- src/App.js
+++ src/App.js
@@ -24,9 +24,13 @@ const App = () => {
       date: new Date(2021, 5, 12),
     },
   ];
+
+  const addExpenseHandler = (expense) => {
+    console.log(expense);
+  }
   return (
     <div className="App">
-      <NewExpense />
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses expense={expenses} />
     </div>
   );
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,14 +1,24 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import './Expenses.css'
 
 const Expenses = (props) => {
+    const [filteredYear, setFilteredYear] = useState('2021');
+    const filterChangeHandler = (selectedYear) => {
+        console.log(selectedYear);
+        setFilteredYear(selectedYear);
+    }
     return (
-        <Card className='expenses'>
-            <ExpenseItem title={props.expense[0].title} amount={props.expense[0].amount} date={props.expense[0].date} />
-            <ExpenseItem title={props.expense[1].title} amount={props.expense[1].amount} date={props.expense[1].date} />
-            <ExpenseItem title={props.expense[2].title} amount={props.expense[2].amount} date={props.expense[2].date} />
-        </Card>
+        <div>
+            <Card className='expenses'>
+                <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
+                <ExpenseItem title={props.expense[0].title} amount={props.expense[0].amount} date={props.expense[0].date} />
+                <ExpenseItem title={props.expense[1].title} amount={props.expense[1].amount} date={props.expense[1].date} />
+                <ExpenseItem title={props.expense[2].title} amount={props.expense[2].amount} date={props.expense[2].date} />
+            </Card>
+        </div>
     );
 }
 
